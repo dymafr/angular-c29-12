8118458ef63edc58320435f14f44bf82080a0127
dymafr <erwan1309@hotmail.com>
2021-10-05 14:55:11 +0200

    2 files modified

--- src/app/features/photos/shared/services/unsplash.service.ts
+++ src/app/features/photos/shared/services/unsplash.service.ts
@@ -1,16 +1,29 @@
 import { Injectable } from '@angular/core';
 import { from, Observable } from 'rxjs';
 import { Photo } from '../interfaces/photo.interface';
+import { createApi } from 'unsplash-js';
 import { map } from 'rxjs/operators';
-import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UnsplashService {
-  constructor(private http: HttpClient) {}
+  private accessKey =
+    'd735ade0117e703f4c8b2ef98cfd27879291d34c8de2d7dd261616f684df435c';
+
+  private unsplash = createApi({
+    accessKey: this.accessKey,
+  });
+
+  constructor() {}
 
   public searchPhotos(search: string): Observable<Photo[]> {
-    return this.http.get<Photo[]>('');
+    return from(
+      this.unsplash.search.getPhotos({ query: search, orientation: 'squarish' })
+    ).pipe(
+      map((result: any) =>
+        result.response?.results.map((r: any) => ({ url: r.urls.small }))
+      )
+    );
   }
 }
